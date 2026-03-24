0f3f77c2da0d7b3b6caef508ca7114f684878076
MashaZenchenkova <zenchenkova.m@mail.ru>
2026-03-24 21:56:13 +0300

    Fix Python file for pylint

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print('Hello from feature branch')
+"""Simple hello world module."""
+
+def main():
+    """Print hello message."""
+    print("Hello from feature branch!")
+
+if __name__ == "__main__":
+    main()
